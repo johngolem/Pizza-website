66634aa6edc148d5056e5db363ea9dff59e4b222
john paul <jpaulopiy@gmail.com>
2021-06-28 14:52:07 +0300

    added js script

--- js/scripts.js
+++ js/scripts.js
@@ -25,3 +25,171 @@ $(document).ready(function(){
             ptopping.push($(this).val());
         });
         console.log(ptopping.join(", "));
+        switch(psize){
+            case "0":
+              price =0;
+            break;
+            case "extra-large":
+               price = 1200;
+               console.log(price);
+             break;
+             case "large":
+               price = 1000;
+               console.log(price);
+             break;
+             case "medium":
+               price = 800;
+               console.log("The price is "+price);
+             break;
+             case "small":
+               price = 600;
+               console.log(price);
+             default:
+               console.log("error"); 
+        switch(pcrust){
+            case "0":
+                cust_price =0;
+            break;
+            case "Neopolitan":
+                cust_price =;
+                console.log(price)
+            break;
+            case "Deeppan":
+                cust_price =;
+                console.log(price)
+            break;
+            case "Flatbread":
+                cust_price =;
+                console.log(price)
+            default:
+                console.log("No price");        
+        }
+        let topping_value = ptopping.length*50;
+    console.log("toppins value" + topping_value);
+
+    if((psize == "0") && (pcrust == "0")){
+      console.log("nothing selected");
+      $("button.proceed").show();
+      $("#information").show();
+      $("div.choise").hide();
+      alert("Please select pizza size and crust"); 
+    }
+    else{
+      $("button.proceed").hide();
+      $("#information").hide();
+      $("div.choise").slideDown(1000);
+    }
+
+    total = price + crust_price + topping_value;
+    console.log(total);
+    let checkoutTotal =0;
+    checkoutTotal = checkoutTotal + total;
+
+    $("#pizzaname").html($(".name option:selected").val());
+    $("#pizzasize").html( $("#size option:selected").val());
+    $("#pizzacrust").html($("#crust option:selected").val());
+    $("#pizzatopping").html(ptopping.join(", "));
+    $("#totals").html(total);
+
+    // ************************************* Add pizza button ************************************************//////
+    $("button.addPizza").click(function(){
+        let pname = $(".name option:selected").val();
+        let psize = $("#size option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let ptopping = [];
+        $.each($("input[name='toppings']:checked"), function(){            
+            ptopping.push($(this).val());
+        });
+        console.log(ptopping.join(", "));
+      switch(psize){
+        case "0":
+          price =0;
+        break;
+        case "extra-large":
+           price = 1200;
+           console.log(price);
+        break;
+        case "large":
+           price = 1000;
+           console.log(price);    
+         break;
+         case "medium":
+           price = 800;
+           console.log("The price is "+price);
+         break;
+         case "small":
+           price = 600;
+           console.log(price);
+         default:
+           console.log("error"); 
+        }
+        switch(pcrust){
+           case "0":
+             crust_price = 0;
+           break;
+           case "Neopolitan":
+             crust_price = 200;
+           break;
+           case "Deeppan":
+             crust_price = 180;
+           break;
+           case "Flatbread":
+             crust_price = 150;
+           break;
+           default:
+             console.log("No price");
+            }
+            let topping_value = ptopping.length*50;
+            console.log("toppins value" + topping_value);
+            total = price + crust_price + topping_value;
+            console.log(total);
+    
+            checkoutTotal = checkoutTotal + total;
+            console.log(checkoutTotal);
+                 //*************************************/ constractor function *******************************************
+      var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
+
+      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+      console.log(newOrder);
+       //*************************************/ constractor function *******************************************
+    });
+    // *****************************************Checkout button
+    $("button#checkout").click(function(){ 
+      $("button#checkout").hide();
+      $("button.addPizza").hide();
+      $("button.deliver").slideDown(1000);
+      $("#addedprice").slideDown(1000);
+      console.log("Your total bills is sh. "+checkoutTotal);
+      $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
+    });
+
+    // ***************************home delivery button
+    $("button.deliver").click(function(){
+      $(".pizzatable").hide();
+      $(".choise h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let deliceryamount= checkoutTotal+100;
+      console.log("You will pay sh. "+deliceryamount+" on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: "+deliceryamount);
+    });
+
+    // ******************************************when one clicks place order button
+    $("button#final-order").click(function(event){
+      event.preventDefault();
+
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliceryamount= checkoutTotal+100;
+      console.log("Final Bill is: "+deliceryamount);
+      let person = $("input#name").val();
+       
+       
+ }
+
+
+
+
