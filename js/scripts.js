e6d82d57259c72519e88dbb9bb015dd7cc766984
john paul <jpaulopiy@gmail.com>
2021-06-28 17:37:55 +0300

    reviewed the js and got it to work

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,12 @@
-var price , crust_price ,topping_price ;
-let total=0;
-function GetPizza(name,size,topping,crust, total){
-    this.name = name;
-    this.size = size;
-    this.topping = topping;
-    this.crust = crust;
-    this.total = total;   
+var price , crust_price, topping_price ;
+let total = 0;
+function Getpizza( name,size,crust,topping, total ){
+  this.name = name;
+  this.size = size;
+  this.crust = crust;
+  this.topping = topping;
+  this.total = total;
 }
-
 
 // proceed button
 $(document).ready(function(){
@@ -17,14 +16,15 @@ $(document).ready(function(){
     //   $("div.choise").slideDown(1000);
     // });
     $("button.proceed").click(function(event){
-        let pname = $(".name option:selected").val();
-        let psize = $("#size option:selected").val();
-        let pcrust = $("#crust option:selected").val();
-        let ptopping = [];
-        $.each($("input[name='toppings']:checked"), function(){            
-            ptopping.push($(this).val());
-        });
-        console.log(ptopping.join(", "));
+     let pname = $(".name option:selected").val();
+     let psize = $("#size option:selected").val();
+     let pcrust = $("#crust option:selected").val();
+     let ptopping = [];
+     $.each($("input[name='toppings']:checked"), function(){            
+         ptopping.push($(this).val());
+     });
+     console.log(ptopping.join(", "));
+  
         switch(psize){
             case "0":
               price =0;
@@ -46,38 +46,39 @@ $(document).ready(function(){
                console.log(price);
              default:
                console.log("error"); 
+        }
         switch(pcrust){
             case "0":
                 cust_price =0;
             break;
             case "Neopolitan":
-                cust_price =;
+                cust_price =200;
                 console.log(price)
             break;
             case "Deeppan":
-                cust_price =;
+                cust_price =180;
                 console.log(price)
             break;
             case "Flatbread":
-                cust_price =;
+                cust_price =150;
                 console.log(price)
             default:
                 console.log("No price");        
         }
         let topping_value = ptopping.length*50;
-    console.log("toppins value" + topping_value);
+    console.log("toppings value" + topping_value);
 
     if((psize == "0") && (pcrust == "0")){
       console.log("nothing selected");
       $("button.proceed").show();
       $("#information").show();
-      $("div.choise").hide();
+      $("div.choice").hide();
       alert("Please select pizza size and crust"); 
     }
     else{
       $("button.proceed").hide();
       $("#information").hide();
-      $("div.choise").slideDown(1000);
+      $("div.choice").slideDown(1000);
     }
 
     total = price + crust_price + topping_value;
@@ -99,30 +100,30 @@ $(document).ready(function(){
         let ptopping = [];
         $.each($("input[name='toppings']:checked"), function(){            
             ptopping.push($(this).val());
-        });
-        console.log(ptopping.join(", "));
-      switch(psize){
+    });
+    console.log(ptopping.join(", "));
+    switch(psize){
         case "0":
           price =0;
         break;
         case "extra-large":
-           price = 1200;
-           console.log(price);
+          price = 1200;
+          console.log(price);
         break;
         case "large":
-           price = 1000;
-           console.log(price);    
-         break;
-         case "medium":
-           price = 800;
-           console.log("The price is "+price);
-         break;
-         case "small":
-           price = 600;
-           console.log(price);
-         default:
-           console.log("error"); 
-        }
+          price = 1000;
+          console.log(price);    
+        break;
+        case "medium":
+          price = 800;
+          console.log("The price is "+price);
+        break;
+        case "small":
+          price = 600;
+          console.log(price);
+        default:
+          console.log("error"); 
+    }
         switch(pcrust){
            case "0":
              crust_price = 0;
@@ -138,19 +139,19 @@ $(document).ready(function(){
            break;
            default:
              console.log("No price");
-            }
-            let topping_value = ptopping.length*50;
-            console.log("toppins value" + topping_value);
-            total = price + crust_price + topping_value;
-            console.log(total);
+    }
+    let topping_value = ptopping.length*50;
+    console.log("toppins value" + topping_value);
+    total = price + crust_price + topping_value;
+    console.log(total);
     
-            checkoutTotal = checkoutTotal + total;
-            console.log(checkoutTotal);
+    checkoutTotal = checkoutTotal + total;
+    console.log(checkoutTotal);
                  //*************************************/ constractor function *******************************************
-      var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
+    var newOrder = new Getpizza(pname, psize, pcrust,ptopping,total);
 
-      $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
-      console.log(newOrder);
+    $("#ordersmade").append('<tr><td id="pizzaname">'+newOrder.name +'</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">'+newOrder.crust + '</td><td id="pizzatopping">'+newOrder.topping+'</td><td id="totals">'+newOrder.total+'</td></tr>');
+    console.log(newOrder);
        //*************************************/ constractor function *******************************************
     });
     // *****************************************Checkout button
@@ -203,9 +204,8 @@ $(document).ready(function(){
     });
    event.preventDefault();
   });
-       
-       
- }
+});
+    
 
 
 
