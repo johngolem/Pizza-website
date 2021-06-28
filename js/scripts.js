ece0ef386e6c491148072e846a45c05f45304c76
john paul <jpaulopiy@gmail.com>
2021-06-28 14:20:49 +0300

    add css and bootstrap  stylesheet

--- js/scripts.js
+++ js/scripts.js
@@ -5,5 +5,23 @@ function GetPizza(name,size,topping,crust, total){
     this.size = size;
     this.topping = topping;
     this.crust = crust;
-    this.total = total;
+    this.total = total;   
 }
+
+
+// proceed button
+$(document).ready(function(){
+    // $("button.proceed").click(function(){
+    //   $("button.proceed").hide();
+    //   $("#information").hide();
+    //   $("div.choise").slideDown(1000);
+    // });
+    $("button.proceed").click(function(event){
+        let pname = $(".name option:selected").val();
+        let psize = $("#size option:selected").val();
+        let pcrust = $("#crust option:selected").val();
+        let ptopping = [];
+        $.each($("input[name='toppings']:checked"), function(){            
+            ptopping.push($(this).val());
+        });
+        console.log(ptopping.join(", "));
