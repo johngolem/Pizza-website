5b63f053f8e813f50dfbffab5d9f43250b4ef665
john paul <jpaulopiy@gmail.com>
2021-06-28 14:53:51 +0300

    finished js

--- js/scripts.js
+++ js/scripts.js
@@ -186,6 +186,23 @@ $(document).ready(function(){
       let deliceryamount= checkoutTotal+100;
       console.log("Final Bill is: "+deliceryamount);
       let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+  
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliceryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+   event.preventDefault();
+  });
        
        
  }
